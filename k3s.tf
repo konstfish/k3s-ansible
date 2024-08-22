03ef555285b51354df2c2ac1b50d99c7950eb58f
David Fischer <david@konst.fish>
2024-08-22 23:29:40 +0200

    feat(tf): init module

---
+++ k3s.tf
@@ -0,0 +1,41 @@
+resource "local_file" "ansible_inventory" {
+  content = templatefile("${path.module}/inventory.tpl", {
+    controller_nodes = var.controller_nodes,
+    worker_nodes     = var.worker_nodes,
+
+    ansible_user    = var.ansible_user,
+    ansible_ssh_key = var.ansible_ssh_key,
+
+    user_name   = var.user_name,
+    github_user = var.github_user,
+
+    k3s_version  = var.cluster_k3s_version,
+    k3s_token    = var.cluster_token,
+    cluster_name = var.cluster_name,
+    cluster_type = var.cluster_type,
+
+    lb_public_address   = var.lb_public_address,
+    lb_internal_address = var.lb_internal_address,
+    lb_interface        = var.lb_interface
+    lb_port             = var.lb_port
+
+    extra_server_args = var.extra_server_args
+    cluster_cidr      = var.cluster_cidr
+    service_cidr      = var.service_cidr
+  })
+
+  filename = "${path.module}/inventory.yml"
+
+  provisioner "local-exec" {
+    command     = <<EOT
+      sleep 10 # wait for nodes to be ready
+      echo "$SSH_PRIVATE_KEY" > artifacts/ssh_key && chmod 600 artifacts/ssh_key
+      ansible-playbook -i inventory.yml playbook/install.yml --extra-vars "kubeconfig_localhost=true kubeconfig_localhost_ansible_host=false"
+    EOT
+    working_dir = "k3s-ansible"
+    environment = {
+      ANSIBLE_HOST_KEY_CHECKING = "false"
+      SSH_PRIVATE_KEY           = nonsensitive(var.ansible_ssh_key)
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,87 @@
+variable "controller_nodes" {
+  type = list(object({
+    name             = string
+    ansible_host     = string
+    ansible_ssh_host = string
+  }))
+}
+
+variable "worker_nodes" {
+  type = list(object({
+    name             = string
+    ansible_host     = string
+    ansible_ssh_host = string
+  }))
+}
+
+variable "ansible_user" {
+  type    = string
+  default = "root"
+}
+
+variable "ansible_ssh_key" {
+  type    = string
+  default = "./artifacts/ssh_key"
+}
+
+variable "user_name" {
+  type = string
+}
+
+variable "github_user" {
+  type = string
+}
+
+variable "cluster_k3s_version" {
+  type = string
+}
+
+variable "cluster_token" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "cluster_type" {
+  type = string
+}
+
+variable "lb_public_address" {
+  type = string
+}
+
+variable "lb_internal_address" {
+  type = string
+}
+
+variable "lb_interface" {
+  type    = string
+  default = "enp7s0"
+}
+
+variable "lb_port" {
+  type    = number
+  default = 6443
+}
+
+variable "extra_server_args" {
+  type    = string
+  default = ""
+}
+
+variable "cluster_cidr" {
+  type    = string
+  default = "10.44.0.0/16"
+}
+
+variable "service_cidr" {
+  type    = string
+  default = "10.45.0.0/16"
+}
+
+variable "ansible_ssh_key" {
+  type      = string
+  sensitive = true
+}
